20d4a33875c3d43b040d9f24deb369e9fc96c41e
Daniel Valls Estella <daniel@upzzle.com>
2017-10-30 17:46:18 +0100

    commit inicial

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -9,7 +9,7 @@ extension Droplet {
         }
 
         get("plaintext") { req in
-            return "Hello, world!"
+            return "Hello, f world!"
         }
 
         // response to requests to /info domain
